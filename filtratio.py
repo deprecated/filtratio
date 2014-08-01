55f3a16643f039b7dc4a4bf752e31ef2c24d31ae
William Henney <will@henney.org>
2014-07-31 23:53:53 -0500

    Multiplets now work

--- filtratio.py
+++ filtratio.py
@@ -10,28 +10,26 @@ air_rest_wavelength = {
     4340: 4340.47,
     4363: 4363.21,
     4861: 4861.63,
-    5007: {5006.843: 2.918, 4958.911: 1.0, 4931.227: 3.88e-4},
+    5007: np.array([5006.843, 4958.911, 4931.227]),
     5755: 5755.08,
     6563: 6562.79,
-    6583: {6583.45: 2.963, 6548.05: 1.0, 6527.23: 5.33e-4},
+    6583: np.array([6583.45, 6548.05, 6527.23]),
     6716: 6716.44,
     6731: 6730.816
 }
 
+multiplet_strengths = {
+    5007: np.array([2.918, 1.0, 3.88e-4]),
+    6583: np.array([2.963, 1.0, 5.33e-4]),
+}
 
 def _get_bandpass(fname):
     return pysynphot.ObsBandpass(fname)
 
 
-def _vacuum_wavelength(wav0, topocentric_velocity):
+def _vacuum_wavelength(wavid, topocentric_velocity):
     "Convert from wav identifier to an accurate (0.01 Ang) vacuum wavelength"
-    wav_or_dict = air_rest_wavelength.get(wav0, float(wav0))
-    try:
-        # case where we have a dict of multiplet components
-        wav = np.array(list(wav_or_dict.keys()))
-    except:
-        # case where we just have a number
-        wav = float(wav_or_dict)
+    wav = air_rest_wavelength.get(wavid, float(wavid))
     return  wav*AIR_REFRACTIVE_INDEX*(1.0 + topocentric_velocity/LIGHTSPEED)
 
 
@@ -63,13 +61,11 @@ if __name__ == "__main__":
     for T, fn in zip(filterset.filter_bandpasses, filterset.filter_ids):
         plt.plot(T.wave, T.throughput, label=fn)
     for line_id, wavs in zip(filterset.line_ids, filterset.line_vac_wavs):
-        for wav0, vacwav in zip(air_rest_wavelength[line_id].keys(),
-                                np.atleast_1d(wavs)):
-            try: 
-                strength = air_rest_wavelength[line_id][wav0]
-            except:
-                strength = 3.0
-            plt.axvline(wav0, 0.0, 0.3*strength, color='r', lw=2)
+        if line_id in multiplet_strengths:
+            for wav, strength in zip(wavs, multiplet_strengths[line_id]):
+                plt.axvline(wav, 0.0, 0.3*strength, color='r', lw=2)
+        else:
+            plt.axvline(wavs, 0.0, 0.9, color='b', lw=2)
 
     plt.xlim(*filterset.wavlimits())
     plt.legend(fontsize='small', loc='upper left')
