353baa0d5ddfbed01f3381a5aa70b1710d64815c
William Henney <will@henney.org>
2014-08-07 18:26:55 -0500

    Add more package metadata to setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,32 @@ from os.path import join, dirname
 
 import nebulio
 
-setup(name='nebulio',
-      version=nebulio.__version__,
-      description='Derive emission line ratios from count rates in HST filters ',
-      longdesc=open(join(dirname(__file__), 'README.md')).read(),
-      url='https://github.com/deprecated/nebulio',
-      author='William Henney',
-      author_email='w.henney@crya.unam.mx',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=[
-          'numpy',
-          'matplotlib',
-          'pysynphot',
-      ],
-      zip_safe=False)
+setup(
+    name='nebulio',
+    version=nebulio.__version__,
+    description='Derive emission line ratios from count rates in HST filters ',
+    longdesc=open(join(dirname(__file__), 'README.md')).read(),
+    url='https://github.com/deprecated/nebulio',
+    author='William Henney',
+    author_email='w.henney@crya.unam.mx',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'pysynphot',
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+)
